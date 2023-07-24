10cb977dffe2f2353f094c007741ebb242a38f11
OmarE20 <omarebeadp@gmail.com>
2023-07-24 11:06:21 -0400

    Progress

--- PhotoEdit.py
+++ PhotoEdit.py
@@ -1,1 +1,14 @@
-print("Hello world")
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = 'C:\Users\omare\OneDrive\Desktop\Old Images'
+pathOut = 'C:\Users\omare\OneDrive\Desktop\New Images'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    cleanName = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{cleanName}_edited.jpg')
