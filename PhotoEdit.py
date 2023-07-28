a760291cdee0c66ba51771574fa27fcaa9fcfe4d
OmarE20 <omarebeadp@gmail.com>
2023-07-28 13:01:37 -0400

    Update 2

--- PhotoEdit.py
+++ PhotoEdit.py
@@ -7,7 +7,11 @@ pathOut = 'C:\Users\omare\OneDrive\Desktop\New Images'
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
 
-    edit = img.filter(ImageFilter.SHARPEN)
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
 
     cleanName = os.path.splitext(filename)[0]
 
